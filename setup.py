8819e212a0bf192ec507536cb81d6ee19a997979
Mostafa Hagar <mostafa@150mL.com>
2026-03-09 16:06:00 -0700

    Conditionally include OpenMP depending on OS

--- setup.py
+++ setup.py
@@ -4,11 +4,24 @@ Cython build script for find_mfs extensions.
 Extensions are built automatically via `pip install -e .` thanks to
 the [build-system] in pyproject.toml including Cython.
 """
+import platform
 import numpy as np
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
 compile_args = ["-O3", "-ffast-math"]
+
+# OpenMP: supported on Linux (gcc) and Windows (MSVC), but not
+# Apple's clang which ships without OpenMP by default.
+if platform.system() == "Darwin":
+    omp_compile_args = []
+    omp_link_args = []
+elif platform.system() == "Windows":
+    omp_compile_args = ["/openmp"]
+    omp_link_args = []
+else:
+    omp_compile_args = ["-fopenmp"]
+    omp_link_args = ["-fopenmp"]
 
 extensions = [
     Extension(
@@ -33,8 +46,8 @@ extensions = [
         "find_mfs.isotopes._isospec",
         sources=["find_mfs/isotopes/_isospec.pyx"],
         include_dirs=[np.get_include()],
-        extra_compile_args=compile_args + ["-fopenmp"],
-        extra_link_args=["-fopenmp"],
+        extra_compile_args=compile_args + omp_compile_args,
+        extra_link_args=omp_link_args,
     ),
 ]
 
